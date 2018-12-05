296a6eae2dcfd270068c55c4cca9e549f65b4f85
sirimatam <45136358+sirimatam@users.noreply.github.com>
2018-12-05 15:02:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if ( sizeof($request_array['events']) > 0 )
 		$reply_message = 'Hi,what is you name';
 		$checkname = 1;
 	}
-	if ($checkname == 1) && ($correct == 0)
+	else if ($checkname == 1) && ($correct == 0)
 	{
 		$reply_message = $text.','.'what can i help you?';
 		$checkname = 0;
