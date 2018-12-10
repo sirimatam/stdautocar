453a3b7f9890ddbefd868831d8fa01b2fad21a8c
sirimatam <45136358+sirimatam@users.noreply.github.com>
2018-12-10 16:36:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,25 @@
 <?php
-
 $db = pg_connect("host=ec2-54-235-193-0.compute-1.amazonaws.com port=5432 dbname=d6ccd2htpr3a7o user=hiaucjuwlowgrh password=2df3a048937c2f6a07847fb6816bf88582290e8249eac587eaf1c5b29e29998a");
 echo $db;
-
 //pg_query($db,"CREATE TABLE Customer1 (cus_id varchar(5) NOT NULL PRIMARY KEY, cus_name varchar(30) NOT NULL, cus_lastname varchar(30) NOT NULL, cus_pic varchar(30) NOT NULL)");
-
-
-//pg_query($db,"INSERT INTO Customer1 (cus_id,cus_name,cus_lastname,cus_pic) VALUES ('C003','AAA','ABC','no')");
+pg_query($db,"UPDATE Customer1 SET cus_id = 'C001' WHERE cus_name = 'Stamp' ");
+pg_query($db,"INSERT INTO Customer1 (cus_id,cus_name,cus_lastname,cus_pic) VALUES ('C002','Pukky','Jungsiri','no')");
 $result = pg_query($db,"SELECT COUNT(*) FROM Customer1");
 $list = pg_fetch_row($result);
 echo  "result = $list[0] <br>";
-
 $result = pg_query($db,"SELECT * FROM Customer1");
 while ($list = pg_fetch_row($result))
 echo  "result = $list[0].$list[1].$list[2].$list[3]<br>";
-
 /*
 pg_query($db,"CREATE TABLE Garage (
 gar_id varchar(10) NOT NULL,
 gar_name varchar(40) NOT NULL),
 gar_tel varchar(10) NOT NULL");
-
-
 pg_query($db,"INSERT INTO Garage VALUES ('g01','อู่คุณ A','0812223333')");
-
 $result = pg_query($db,"SELECT * FROM Garage");
 $list = pg_fetch_row($result);
 echo "result = $list[0]";
-
 */
-
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
 $ACCESS_TOKEN = 'L0246N0Dd1KuwHuzqr88jOCehjzvrytHUf+Yrdq5cD6omLdxDQGGFcvQBIMemj5NzlLRmgGiFA2sTLoxwN5PVxXN2QMwMf3Y45fLcYsi6wI2Sw7BoqUzGU4kCU6I9NJwsVlnibO8YL6Id1U9rHEkowdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
@@ -41,65 +31,28 @@ if ( sizeof($request_array['events']) > 0 )
  {
   $reply_message = '';
   $reply_token = $event['replyToken'];
-if ( sizeof($request_array['events']) > 0 )
-{
- foreach ($request_array['events'] as $event)
- {
-  $reply_message = '';
-  $reply_token = $event['replyToken'];
   if ( $event['type'] == 'message' ) 
   {
    if( $event['message']['type'] == 'text' )
    {
     $text = $event['message']['text'];
-	$car = array('"1"',"1","ค้นหารถ","รถ","หารถ","ยี่ห้อรถ");
-	$tel = array("tel","เบอร์","หาเบอร์","2",'"2"',"โทร","เบอร์โทร");
-	   
-	$check = 0;
 	
-	foreach ($car as $value)
+	$greeting = array('Hi','Hello','ดีจ้า','สวัสดี','สวัสดีครับ');
+	
+	$correct = 0;
+	foreach ($greeting as $value)
 	{
-		if($text == $value)
+		if ($text == $value)
 		{
-			$check=1;
+			$correct = 1;
 		}
 	}
-	foreach ($tel as $value)
+	if ($correct == 1)
 	{
-		if($text == $value)
-		{
-			$check=2;
-		}
+		$reply_message = 'Hi,what is you name';
 	}
-	
-	/*if($check ==1)
-	{
-		$result = pg_query($db,"SELECT COUNT(*) FROM Customer");
-		$list = pg_fetch_row($result);
-		$reply_message = " result = $list[0]";
-	}
-	elseif($check ==2)
-	{
-		pg_query($db,"INSERT INTO Customer VALUES ('c01','Somkit')");
-		$reply_message = 'อู่คุณวิชัย 023334444';
-	}*/
-	elseif($text==3)
-	{
-		$reply_message = 'บัญชีประจำเดือน ธันวาคม 2561, ค่าใช้จ่าย 3,000 บาท, เงินสดหมุนเวียน 400,000 บาท ต้องการเพิ่มข้อมูล กด "4"';
-	}
-	/*elseif($text==4)
-	{
-		$result = pg_query($db,"SELECT Customer1.cus_name FROM Customer1");
-		$custlist ='';
-		while ($list = pg_fetch_row($result))
-		$custlist += $list
-		$reply_message = "$custlist";
-	}*/
-	
-		
-	   
 	else
-		$reply_message = 'พิมพ์ "1" เมื่อต้องการค้นหารถ, พิมพ์ "2" เมื่อต้องการค้นหาเบอร์ติดต่อของบริษัท, พิมพ์ "3" เมื่อต้องการตรวจสอบการเงิน,พิมพ์ "4" เมื่อต้องการเรียกดูข้อมูลลูกค้า' ;
+	$reply_message = 'why dont you say hello to me';
    }
    else
     $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
@@ -121,9 +74,7 @@ if ( sizeof($request_array['events']) > 0 )
   }
  }
 }
-
 echo "OK";
-	 
 function send_reply_message($url, $post_header, $post_body)
 {
  $ch = curl_init($url);
