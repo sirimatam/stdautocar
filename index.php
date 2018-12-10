4a655885620861331f7d01e6302fcdd76dc97b0e
sirimatam <45136358+sirimatam@users.noreply.github.com>
2018-12-10 19:18:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -67,6 +67,18 @@ if ( sizeof($request_array['events']) > 0 )
 		}
 		$reply_message = "$custlist";
 	}
+	elseif (substr($text,0,6) =='addcar')
+	{
+		$cardata = explode(" ",$text);
+		pg_query($db,"INSERT INTO Customer1 (cus_id,cus_name,cus_lastname,cus_pic) VALUES ($cardata[1],$cardata[2],$cardata[3],$cardata[4])");
+		$result = pg_query($db,"SELECT cus_name FROM Customer1");
+		while ($list = pg_fetch_row($result))
+		{
+			$cust = $list[0]."\n";
+			$custlist .= $cust;
+		}
+		$reply_message = "$custlist";
+	}
 	else
 	$reply_message = 'why dont you say hello to me';
    }
