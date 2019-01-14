5c5fe09b66fb292cbcde446425b50b413981e876
sirimatam <45136358+sirimatam@users.noreply.github.com>
2019-01-14 11:23:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -164,7 +164,7 @@ function send_reply_message($url, $post_header, $post_body)
  curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
- curl_setopt($ch, CURLOPT_ENCODING, "");
+ 
  $result = curl_exec($ch);
  curl_close($ch);
  return $result;
